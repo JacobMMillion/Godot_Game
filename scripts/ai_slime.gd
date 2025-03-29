5a6bad6f7f4e8d040cf93799e26f5bd86fba0cd7
Jacob Million <jm5530@columbia.edu>
2025-03-29 16:30:29 -0400

    update ai slime not be able to go through mid blocks

--- scripts/ai_slime.gd
+++ scripts/ai_slime.gd
@@ -1,21 +1,24 @@
 extends Node2D
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+# Adjust the path below to match where your player node is in the scene tree
+@onready var player: Node2D = get_node("/root/level1/player")
 
-const SPEED = 60
-var direction = 1
+const SPEED = 40
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	# Make sure the player exists
+	if not player:
+		return
 	
-	if ray_cast_right.is_colliding():
-			direction = -1
-			animated_sprite.flip_h = true
-		
-	if ray_cast_left.is_colliding():
-			direction = 1
-			animated_sprite.flip_h = false
-	
-	position.x += direction * delta * SPEED
+	# Check the player's position relative to the enemy
+	if player.position.x < position.x and not ray_cast_left.is_colliding():
+		# Move left
+		position.x -= SPEED * delta
+		animated_sprite.flip_h = true
+	elif player.position.x > position.x and not ray_cast_right.is_colliding():
+		# Move right
+		position.x += SPEED * delta
+		animated_sprite.flip_h = false
