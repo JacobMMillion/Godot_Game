485b81c2b553a41ba2b8202c2a99b21340aa156d
Abenezer Amanuel <abenezeramanuel@Abenezers-MacBook-Pro-9.local>
2025-05-08 14:55:08 -0400

    adjusted coin hitbox, fixed bugs

--- scenes/level_2.gd
+++ scenes/level_2.gd
@@ -14,23 +14,20 @@ func _ready():
 	timer.timeout.connect(_on_Timer_timeout)
 	timer.start()  
 	time_label.text = "Time: " + str(int(timer.wait_time))
-	player.speak("Time for some shooting practice!", 3.0)
 	player.speak("I need my gun...", 3.0)
-	
-func end_level():
-	get_tree().reload_current_scene()
-	
-
 
 func _process(delta):
 	time_label.text = "Time: " + str(int(timer.time_left))
 	if timer.time_left > 0 and timer.time_left < 10:
 		time_label.add_theme_color_override("font_color", Color.RED)
 	else: 
-		time_label.add_theme_color_override("font_color", Color.WHITE)		
+		time_label.add_theme_color_override("font_color", Color.WHITE)	
+			
 func _on_Timer_timeout():
-	get_tree().change_scene_to_file("res://scenes/game_over.tscn")
-
+	player.die()
 
 func _on_level_3_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level_3.tscn")
+	
+func end_level():
+	get_tree().reload_current_scene()
